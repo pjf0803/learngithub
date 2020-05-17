663c2374a96531520b5ac0d9734ba5eb26f14c7d
pjf0803 <171892359@qq.com>
2020-05-18 00:13:25 +0800

    321

--- index.js
+++ index.js
@@ -8,19 +8,3 @@ console.log(31414312)
 
 console.log(31414312)
 
-
-console.log(31414312)
-console.log(31414312)
-console.log(31414312)
-
-
-console.log(31414312)
-console.log(31414312)
-
-
-
-console.log(31414312)
-
-
-console.log(31414312)
-console.log(31414312)
