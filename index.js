063977007dd08a4a9db67dc1282724033aef6bfc
pjf0803 <171892359@qq.com>
2020-05-18 00:06:05 +0800

    321

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 console.log(31414312)
 console.log(31414312)
 
-console.log(31414312)
-console.log(31414312)
 
-console.log(31414312)
-console.log(31414312)
-
-console.log(31414312)
-console.log(31414312)
-
-console.log(31414312)
-console.log(31414312)
 
