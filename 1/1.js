91192ee065fc337ac91171a2a1a6b43f3a1e865d
pengjunfeng <pengjunfeng@zyzjia.com>
2020-05-26 23:49:10 +0800

    132

--- 1/1.js
+++ 1/1.js
@@ -2,43 +2,5 @@ console.log(232432)
 console.log(31414312)
 
 
-console.log(31414312)
-console.log(31414312)
 
-
-
-console.log(31414312)
-
-
-console.log(31414312)
-
-
-console.log(31414312)
-console.log(31414312)
-
-
-
-console.log(31414312)
-
-
-console.log(31414312)
-
-
-console.log(31414312)
-console.log(31414312)
-
-
-
-console.log(31414312)
-
-
-console.log(31414312)
-
-
-console.log(31414312)
-console.log(31414312)
-
-
-
-console.log(31414312)
 
