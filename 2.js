350f90fe0821ccec45022ea334f6aeddc24e3d7d
pjf0803 <30982940+pjf0803@users.noreply.github.com>
2021-04-22 13:53:48 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-ajfhdaslkjfdsakjfhdasjlkfhdlsajk/ Return Home随记
+
+//ajfhdaslkjfdsakjfhdasjlkfhdlsajk/ Return Home随记
 // CnBlogsHomeNew PostContactAdminSubscription订阅Posts - 87  Articles - 0  Comments - 17  Views - 19万 
 // 删除本地git的远程分支和远程删除git服务器的分支
 // 新建分支:
